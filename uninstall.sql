6a27478e684294cafca97586087065fc05c475f9
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-11-22 14:04:03 +0100

    add uninstallation via Ffuenf_MageTrashApp

--- uninstall.sql
+++
@@ -1,4 +0,0 @@
--- add table prefix if you have one
-DELETE FROM core_config_data WHERE path like 'categoryproductsortbackend_setup/%';
-DELETE FROM core_config_data WHERE path = 'advanced/modules_disable_output/Ffuenf_CategoryProductSortBackend';
-DELETE FROM core_resource WHERE code = 'categoryproductsortbackend_setup_setup';
